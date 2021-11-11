fdfc7e1280a3e93fa69926a02360120e95582e68
Gemmabauza <93371094+Gemmabauza@users.noreply.github.com>
2021-11-11 11:58:08 +0100

    Add files via upload

--- LLETRES.sh
+++ LLETRES.sh
@@ -1,10 +1,10 @@
-LLETRES=( "h" "s" "j" "i" "h" "e" "a" "u" "h" )
+LLETRES=( "h" "s" "j" "i" "h" "H" "e" "a" "u" "h" )
 conta=0
 for var in ${LLETRES[@]}
 do 
-    if [[ $var == "h" ]]
+    if [[ $var == "h" ]] || [[ $var == "H" ]]
     then 
         conta=$(($conta+1))
     fi
  done   
- echo $conta
+ echo "El nombre de h o H és $conta"
