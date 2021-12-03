0f940209f87972abc27c630305d9aa95aae2b027
Gemmabauza <93371094+Gemmabauza@users.noreply.github.com>
2021-12-03 09:41:51 +0100

    Add files via upload

---
+++ quadrat2.sh
@@ -0,0 +1,20 @@
+#GemmaBauzàArtigues
+
+echo "Digues un número"
+read numero
+a=0
+num1=1
+suma=0
+
+for (( i=0; i<numero; j++ ))
+do
+    for (( j=1; j<numero; j++ ))
+    do
+        printf $suma
+    if [ $sum -lt 9 ]
+        then 
+        let summa=$suma+$num1
+    else suma=o
+    fi
+    done
+done
