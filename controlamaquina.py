9362c34266b7cf07b781536726b6c97db3305925
Gemmabauza <93371094+Gemmabauza@users.noreply.github.com>
2022-01-27 12:06:06 +0100

    Create controlamaquina.py

---
+++ controlamaquina.py
@@ -0,0 +1,10 @@
+#Gemma Bauzà Artigues
+"""
+Una aplicació de seguretat controlaMaquina reb  una llistes de valors que indiquen les temperatures d’una màquina. 
+A cada rebuda de dades l’aplicació ha de imprimir el valor màxim i el mínim. També ha d’imprimir el nombre de vegades que ha superat el valor 3 (sols una vegada superat i desprès baixa i el torna superar, són dos pics).
+Exemples de valors:
+Entrada de valors: [1,2,4,4,5,6,7,1,2,5]
+Sortida de valors: “màxim: 7, mínim: 1, variat: 2
+Entrada de valors: [1,4,1,4,1,4]
+Sortida de valors: “màxim: 4, mínim: 1, variat: 3
+"""
